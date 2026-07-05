864a33146fd64b200b269ddd781b9efce53397cf
Kasper <kslt007@gmail.com>
2026-07-05 23:03:52 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Testet fungerar!'));
-
-app.listen(3007, () => {
-    console.log('Minimal testserver startad på port 3007');
-});
